a45cb4fb3bf414302bf7e750beb53f30ad5b50d0
oskar <mroscar005@gmail.com>
2025-03-28 13:24:20 +0100

    koniec zajęć

---
+++ Zadanie 6.tab/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_6.tab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //6. Zadeklaruj tablicę zawierającą nazwy dni tygodnia. Wypisz elementy tablicy za pomocą pętli
+            //foreach.
+            string[] dni = new string [] { "poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota", "niedziela" };
+            foreach (string elem in dni) 
+            Console.Write("{0} ", elem);
+            Console.ReadKey(true);
+
+
+        }
+    }
+}
