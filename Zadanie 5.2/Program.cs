88a95d4e429bf1ce62344e9ff7c31b076509380f
oskar <mroscar005@gmail.com>
2025-04-11 12:09:23 +0200

    koniec zajęc

---
+++ Zadanie 5.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_5._2
+{
+    class Program
+    {
+        static int liczba_pierwsza(int liczba)
+        {
+            //Napisz funkcję, która sprawdza czy jej argument jest liczbą pierwszą. Jako wartość funkcja
+            //powinna zwracać 1 dla liczb pierwszych i 0 dla pozostałych liczb.Przetestuj działanie
+            //utworzonej funkcji.
+            int i;
+            for (i = 1; i <= liczba / 2; i++)
+                if (liczba % i == 0)
+                    return 0;
+            return 1;
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Czy liczba 20 jest liczbą pierwszą: {0}", liczba_pierwsza(20));
+                Console.WriteLine("Czy liczba 23 jest liczbą pierwszą: {0}", liczba_pierwsza(23));
+                Console.ReadKey(true);
+            }
+
+
+        }
+    }
+}
